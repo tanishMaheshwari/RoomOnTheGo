2cbd004262d8f0c5c75b111263474334aab33215
Tanish Maheshwari <75633572+tanishMaheshwari@users.noreply.github.com>
2024-04-30 21:54:08 +0530

    Removed unnecesary files

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const methodOverride = require("method-override");
 const port = 2000;
 const path = require("path");
 
-// app.use("/images", express.static(path.join(__dirname, "images")));
 app.use(express.static("public"));
 const MONGO_URL = "mongodb://localhost:27017/roomonthego";
 
